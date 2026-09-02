b16d793312098cec6f62e873e746ebc06626260a
MohammadAli0096 <mohammadaliattari92@gmail.com>
2026-09-02 13:00:18 +0500

    Hallo World

--- 08_multiplication_Table_Generator.dart
+++
@@ -1,7 +0,0 @@
-// Ask the user for a number and print its table from 1 to 10:
-void main() {
-  int number = 5;
-  for (var i = 1; i <= 10; i++) {
-    print("$number x $i = ${number*i}");
-  }
-}
--- 12_simple Interest Calculator.dart
+++
@@ -1,8 +0,0 @@
-// Create a function that takes principal, rate, and time, and calculates Simple Interest: ini Copy Edit
-void main() {
-  int principal = 20;
-  int rate = 10;
-  int time = 30;
-  int calculate = principal * rate * time;
-  print("totel  :$calculate");
-}
